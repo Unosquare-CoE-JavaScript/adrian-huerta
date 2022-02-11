217740059604875dc937c1a74d1b059074d86835
Adrian Huerta <adrian.huerta@unosquare.com>
2022-02-11 10:58:31 -0600

    TypeScript 2022 Edition | Section 2 | string, number and boolean types

--- typescript_2022_edition/section_2/app.js
+++ typescript_2022_edition/section_2/app.js
@@ -1,1 +1,27 @@
-console.log('hello again');
+/**
+ * Union types
+ * We can have different types, to achieve that, we use the pipe (|) to separate the types that we want.
+ * Example of a variable that can be a number or a string: number | string
+ */
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combineAges = combine(12, 23);
+console.log(combineAges);
+var combineNames = combine('Angel', 'Adrian');
+console.log(combineNames);
+function greetings(user) {
+    console.log("Hi my name is ".concat(user.name, " and I'm ").concat(user.age, " years old"));
+}
+var user = {
+    name: 'Angel',
+    age: 28
+};
+greetings(user);
