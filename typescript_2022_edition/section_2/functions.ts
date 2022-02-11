243cc99cd67daec796cbe221e19c65476a0210a2
Adrian Huerta <adrian.huerta@unosquare.com>
2022-02-11 11:47:24 -0600

    TypeScript 2022 Edition | Section 2 | Function types

--- typescript_2022_edition/section_2/functions.ts
+++ typescript_2022_edition/section_2/functions.ts
@@ -4,19 +4,27 @@
  * When a function doesn't return anything, it has de return type: void.
  * When a function has de return keyword but it doesn't return anything, it has de return type: undefined.
  */
-function add(n1, n2) {
-    return n1 + n2;
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
-function printResult(result) {
-    console.log("Result is: ".concat(result));
+
+function printResult(result: number): void {
+  console.log(`Result is: ${result}`);
 }
-printResult(add(3, 5));
+
+printResult(add(3,5));
+
 // ----------------------------------------------------------------
+
 /**
  * Function types
  * Allow us to describe which type of function specifically we want
  */
+
 // We tell TS that combineValues is a variable of type function, and this function has two parameters, both numbers and returns a number
-var combineValues;
+let combineValues: (a: number, b: number) => number;
+
 combineValues = add;
-console.log(combineValues(8, 8));
+
+console.log(combineValues(8,8));
