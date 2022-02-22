b4bd810fefc8b8c8479e89e2490a90e28d81184b
Adrian Huerta <adrian.huerta@unosquare.com>
2022-02-22 12:32:00 -0600

    React testing | Sundae on Demand | Code & test Alert banner

--- react_testing/sudaes-on-demand/src/pages/entry/Options.jsx
+++ react_testing/sudaes-on-demand/src/pages/entry/Options.jsx
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 import ScoopOption from './ScoopOption';
 import ToppingOptions from './ToppingOptions';
+import AlertBanner from '../common/AlertBanner';
 import axios from 'axios';
 
 import Row from 'react-bootstrap/Row';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {});
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOptions;
 
