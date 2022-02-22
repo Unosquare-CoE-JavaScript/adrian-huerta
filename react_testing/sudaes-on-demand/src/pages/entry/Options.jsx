2035389046f41785d0294d2e5672ffe317ca08e8
Adrian Huerta <adrian.huerta@unosquare.com>
2022-02-22 14:53:44 -0600

    React testing | Sundae on Demand | Creating custom context hook and wrapped in an provider

--- react_testing/sudaes-on-demand/src/pages/entry/Options.jsx
+++ react_testing/sudaes-on-demand/src/pages/entry/Options.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
 import ScoopOption from './ScoopOption';
 import ToppingOptions from './ToppingOptions';
 import AlertBanner from '../common/AlertBanner';
-import axios from 'axios';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../context/OrderDetails';
 
 import Row from 'react-bootstrap/Row';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   useEffect(() => {
     axios
@@ -23,12 +27,29 @@ const Options = ({ optionType }) => {
   }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOptions;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
-    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
+    />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
