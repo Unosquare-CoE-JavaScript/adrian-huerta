3301e9bc7773a7501283d9561e7104401529b031
Adrian Huerta <adrian.huerta@unosquare.com>
2022-02-28 18:27:45 -0600

    React 18 | Nested suspense

--- react_18/concurrent-rendering-adventures-in-react-18/src/App.tsx
+++ react_18/concurrent-rendering-adventures-in-react-18/src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutes } from './components/AppRoutes';
-import { NavBar } from './components/NavBar';
+import { AppRoutes } from "./components/AppRoutes";
+import { Loading } from "./components/Loading";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <NavBar />
-        <AppRoutes />
+        <Suspense fallback={<Loading />}>
+          <AppRoutes />
+        </Suspense>
       </BrowserRouter>
     </div>
   );
--- react_18/concurrent-rendering-adventures-in-react-18/src/components/users/UserDetails.tsx
+++ react_18/concurrent-rendering-adventures-in-react-18/src/components/users/UserDetails.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { ErrorFallback } from "../ErrorFallback";
+import { Loading } from "../Loading";
 import { AccountDetails } from "./AccountDetails";
 import { MovieDetails } from "./MovieDetails";
 
@@ -11,14 +13,16 @@ interface Props {
 export function UserDetails({ userId, movieId }: Props) {
   return (
     <div>
-      <h4 className="text-center mt-5">User details</h4>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <AccountDetails userId={userId} />
-      </ErrorBoundary>
-      <h4 className="text-center mt-5">Favorite movie</h4>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <MovieDetails movieId={movieId} />
-      </ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <h4 className="text-center mt-5">User details</h4>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <AccountDetails userId={userId} />
+        </ErrorBoundary>
+        <h4 className="text-center mt-5">Favorite movie</h4>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <MovieDetails movieId={movieId} />
+        </ErrorBoundary>
+      </Suspense>
     </div>
   );
 }
