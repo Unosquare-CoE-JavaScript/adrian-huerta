7b09b85c8521b51dd9f8b812bd76e3b8c2206375
Adrian Huerta <adrian.huerta@unosquare.com>
2022-08-25 16:58:41 -0500

    Basic unit test

--- testing-workshop-fem/server/src/utils/__tests__/auth.todo.js
+++ testing-workshop-fem/server/src/utils/__tests__/auth.todo.js
@@ -1,27 +1,28 @@
+import { isPasswordAllowed, userToJSON } from '../auth'
+
 test('isPasswordAllowed only allows some passwords', () => {
-  // here's where I'll demo things for you :)
+  expect(isPasswordAllowed('')).toBe(false)
+  expect(isPasswordAllowed('dfsdfsdf')).toBe(false)
+  expect(isPasswordAllowed('24234234')).toBe(false)
+  expect(isPasswordAllowed('ssd4sdf5w.')).toBe(true)
 })
 
 test('userToJSON excludes secure properties', () => {
-  // Here you'll need to create a test user object
-  // pass that to the userToJSON function
-  // and then assert that the test user object
-  // doesn't have any of the properties it's not
-  // supposed to.
-  // Here's an example of a user object:
-  // const user = {
-  //   id: 'some-id',
-  //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
-  //   // ↓ below are properties which shouldn't
-  //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
+  const safeUser = {
+    id: 'some-id',
+    username: 'sarah',
+  }
+
+  const user = {
+    ...safeUser,
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+
+  const jsonUser = userToJSON(user)
+  expect(jsonUser).toEqual(safeUser)
 })
 
 //////// Elaboration & Feedback /////////
